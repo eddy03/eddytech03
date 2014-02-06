df4fdbec3949bcf47e93c3703ef83bba7d3785cf
eddy <eddytech03@gmail.com>
2014-02-06 17:26:49 +0800

    add database configuration setting

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => '127.0.0.1',
+			'database'  => 'website',
+			'username'  => 'webdb_user',
+			'password'  => 'eddy123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
