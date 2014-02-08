ccb18531aecca56ab252ec7a9263ebecbcaf7651
eddytech03 <eddytech03@gmail.com>
2014-02-08 20:59:49 +0800

    Add more control to eddy

--- app/views/ssh/eddy.blade.php
+++ app/views/ssh/eddy.blade.php
@@ -9,4 +9,10 @@ eddytech03.com
 <br />
 <br />
 <button class="btn btn-primary" id="pull" system="spaghetti"><i class="fa fa-download fa-fw"></i> Pull Spaghetti</button> <code>git pull</code>
+<br />
+<br />
+<button class="btn btn-primary" id="pull" system="ewakaf"><i class="fa fa-download fa-fw"></i> Pull eWakaf</button> <code>git pull</code>
+<br />
+<br />
+<button class="btn btn-primary" id="pull" system="vespa"><i class="fa fa-download fa-fw"></i> Pull Vespa</button> <code>git pull</code>
 @endsection
