27522814444d68fca1a206db511027fca248e798
eddytech03 <eddytech03@gmail.com>
2014-02-08 15:37:37 +0800

    define server production

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('eddydyla'),
+    'local' => array('eddydyla'),
+    'production' => array('eddytech03')
 
 ));
 
