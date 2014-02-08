1d73a4dccdd415b973ee9aed8dad47665d8bfc2c
eddytech03 <eddytech03@gmail.com>
2014-02-08 21:05:19 +0800

    fix error multiple class in element

--- app/views/ssh/eddy.blade.php
+++ app/views/ssh/eddy.blade.php
@@ -5,14 +5,14 @@ eddytech03.com
 @endsection
 
 @section('cmd')
-<button class="btn btn-primary" class="pull" cmd="git pull" system="website"><i class="fa fa-download fa-fw"></i> Pull Website</button> <code>git pull</code>
+<button class="btn btn-primary pull" cmd="git pull" system="website"><i class="fa fa-download fa-fw"></i> Pull Website</button> <code>git pull</code>
 <br />
 <br />
-<button class="btn btn-primary" class="pull" cmd="git pull" system="spaghetti"><i class="fa fa-download fa-fw"></i> Pull Spaghetti</button> <code>git pull</code>
+<button class="btn btn-primary pull" cmd="git pull" system="spaghetti"><i class="fa fa-download fa-fw"></i> Pull Spaghetti</button> <code>git pull</code>
 <br />
 <br />
-<button class="btn btn-primary" class="pull" cmd="git pull" system="ewakaf"><i class="fa fa-download fa-fw"></i> Pull eWakaf</button> <code>git pull</code>
+<button class="btn btn-primary pull" cmd="git pull" system="ewakaf"><i class="fa fa-download fa-fw"></i> Pull eWakaf</button> <code>git pull</code>
 <br />
 <br />
-<button class="btn btn-primary" class="pull" cmd="git pull" system="vespa"><i class="fa fa-download fa-fw"></i> Pull Vespa</button> <code>git pull</code>
+<button class="btn btn-primary pull" cmd="git pull" system="vespa"><i class="fa fa-download fa-fw"></i> Pull Vespa</button> <code>git pull</code>
 @endsection
--- app/views/ssh/vespa.blade.php
+++ app/views/ssh/vespa.blade.php
@@ -5,5 +5,5 @@ vespa.eddytech03.com
 @endsection
 
 @section('cmd')
-<button class="btn btn-primary" class="pull" cmd="git pull" system="vespa"><i class="fa fa-download fa-fw"></i> Pull</button> <code>git pull</code>
+<button class="btn btn-primary pull" cmd="git pull" system="vespa"><i class="fa fa-download fa-fw"></i> Pull</button> <code>git pull</code>
 @endsection
