cfbaa7d1e8e9461039938f6ce4d55184f770f238
eddytech03 <eddytech03@gmail.com>
2014-02-10 23:00:39 +0800

    articles full width

--- app/views/contents/articles.blade.php
+++ app/views/contents/articles.blade.php
@@ -9,23 +9,24 @@
 <style>
     #disqus_thread {
         margin-top: 20px;
-        background: #fff;
-        padding: 15px 15px 25px 15px;
-        -webkit-border-radius: 10px 10px 10px 10px;
-        border-radius: 10px 10px 10px 10px;
-        -webkit-box-shadow: 0 3px 2px 1px #ccc;
-        box-shadow: 0 3px 2px 1px #ccc;
+        padding-top: 10px;
+    }
+    .calendars {
+        margin-bottom: 5px;
     }
 </style>
 @endsection
 
 @section('content')
 <div class="row">
-    <div class="col-md-10">
+    <div class="col-xs-12 text-right calendars">
+        <i class="fa fa-calendar fa-fw"></i> Artikel ini dicipta pada : {{ date('d-m-Y', strtotime($articles->created_at)) }}
+    </div>
+    <div class="col-xs-12">
         <div class="read-articles">
             {{ $markdown }}
         </div>
-        <div id="disqus_thread"></div>
+        <div id="disqus_thread" class="read-articles"></div>
         <script type="text/javascript">
             /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
             var disqus_shortname = 'eddytech03'; // required: replace example with your forum shortname
@@ -39,10 +40,10 @@
         </script>
         <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
         <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-    
-    </div>
-    <div class="col-md-2">
-        <i class="fa fa-calendar fa-fw"></i> {{ date('d-m-Y', strtotime($articles->created_at)) }}
+        <hr />
+        <p class="lead">
+            <i class="fa fa-list fa-fw"></i> Artikel lain <i class="fa fa-ellipsis-v fa-fw"></i>
+        </p>
     </div>
 </div>
 @endsection
