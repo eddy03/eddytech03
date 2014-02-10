79adc3f427a72b6e1cbd455b8cb38e52fe5509e3
eddytech03 <eddytech03@gmail.com>
2014-02-10 22:14:44 +0800

    add google analytics

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,6 +10,16 @@
         {{ HTML::style('assets/css/style.css') }}
         
         @yield('style')
+        
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-44462725-1', 'eddytech03.com');
+            ga('send', 'pageview');
+        </script>
     </head>
     
     <body>
