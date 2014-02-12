045300a74ca6f2aae3549c95530d5f02278a4be4
eddy <eddytech03@gmail.com>
2014-02-12 13:51:19 +0800

    Development on about us and project

--- app/views/contents/project.blade.php
+++ app/views/contents/project.blade.php
@@ -5,12 +5,26 @@ Web developer
 @endsection
 
 @section('style')
+<style>
+    .split {
+        height: 50px;
+    }
+</style>
 @endsection
 
 @section('content')
+<div class="page-header remove-top-margin">
+    <h3 class="remove-margin">Senarai projek / portfolio / playground</h3>
+</div>
 <div class="row">
     <div class="col-xs-12">
-        List of my project / portfolio / playground
+        <ol>
+            <li>spaghetti system <span class="text-danger"><i class="fa fa-warning fa-fw"></i> Development phase!</span></li>
+            <li>SCMS system <span class="text-danger"><i class="fa fa-warning fa-fw"></i> Development phase!</span></li>
+            <li>eWakaf system <span class="text-danger"><i class="fa fa-warning fa-fw"></i> Development phase!</span></li>
+        </ol>
+        <div class="split"></div>
+        * Kandungan maklumat didalam bahagian ini masih didalam pembinaan.
     </div>
 </div>
 @endsection
