240a7ab8ca0fd0ccd2bf6366f50bfb849282f2c5
eddy <eddytech03@gmail.com>
2014-02-14 10:52:32 +0800

    Design footer

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -8,10 +8,26 @@
         {{ HTML::style('components/bootswatch/yeti/bootstrap.min.css') }}
         {{ HTML::style('components/font-awesome/css/font-awesome.min.css') }}
         {{ HTML::style('components/animate.css/animate.min.css') }}
-        {{ HTML::style('assets/css/style.css?id=101') }}
-        
+        {{ HTML::style('assets/css/style.css?id=102') }}        
         @yield('style')
+    </head>
+    
+    <body>
+        @include('menu.master')
         
+        <div class="page-wrap">
+            <div class="container">
+                @yield('content')
+            </div>
+        </div>
+        
+        <footer class="site-footer">
+            @include('layouts.footer')
+        </footer>
+        
+        {{ HTML::script('components/jquery/jquery.min.js') }}
+        {{ HTML::script('components/bootstrap/dist/js/bootstrap.min.js') }}
+        @yield('script')
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -21,18 +37,6 @@
             ga('create', 'UA-44462725-1', 'eddytech03.com');
             ga('send', 'pageview');
         </script>
-    </head>
-    
-    <body>
-        @include('menu.master')
-        
-        <div class="container">
-            @yield('content')
-        </div>        
-        
-        {{ HTML::script('components/jquery/jquery.min.js') }}
-        {{ HTML::script('components/bootstrap/dist/js/bootstrap.min.js') }}
-        @yield('script')
         <script>
             $(document).ready(function() {
                 $('.navbar-brand').addClass('animated rollIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
