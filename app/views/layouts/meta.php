19c8fd6c5e25aa45e768ec3a3f4fc12ee9a36c58
eddy <eddytech03@gmail.com>
2014-02-14 14:52:37 +0800

    add meta tags

---
+++ app/views/layouts/meta.php
@@ -0,0 +1,10 @@
+<meta name="description" content="eddytech03.com merupakan sebuah laman maya yang akan membantu anda didalam menghasilkan laman sesawang serta sistem web yang kemas, canggih, terkini serta menarik dengan menggunakan teknologi terkini."/>
+<meta name="keywords" content="Teknologi, Laman maya, software, internet, laravel, pengaturcaraan, programming, code, sumber terbuka, efisen, canggih, dunia maya, blog teknologi, bootstrap, css, php framework, nginx, php, eloquent, blade, hi tech, vagrant, komputer, jquery, MIT, github, version control, MVC, feed, Dota2, laravel php framework, bootstrap css framework, jquery javascript framework" />
+<link rel="canonical" href="http://www.eddytech03.com/"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="eddytech03.com - Ilmu adalah segala-galanya"/>
+<meta property="og:description" content="eddytech03.com merupakan sebuah laman maya yang akan membantu anda didalam menghasilkan laman sesawang serta sistem web yang kemas, canggih, terkini serta menarik dengan menggunakan teknologi yang ada."/>
+<meta property="og:url" content="http://www.eddytech03.com/"/>
+<meta property="og:site_name" content="eddytech03.com"/>
+<meta property="article:publisher" content="https://www.eddytech03.com/tentang"/>
